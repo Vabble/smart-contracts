1d778faa56f27b11505396626106561f1d35569c
devbest555 <martinbenka26@gmail.com>
2023-08-10 08:31:08 +0200

    deploy to polygon mainnet

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,11 +4,13 @@ const { BigNumber } = ethers;
 const NETWORK = 'mumbai';
 const ZERO_ADDRESS = ethers.constants.AddressZero;
 const CONFIG = {
-  oldVAB: "0x5cBbA5484594598a660636eFb0A1AD953aFa4e32", // vab
-  newVAB: "0x53BeF80E0EBE5A89dfb67782b12435aBeB943754", // exm
+  // usdcAdress: "0x7493d25DAfE9dA7B73Ffd52F009D978E2415bE0c",
+  // usdtAdress: "0x47719C2b2A6853d04213efc85075674E93D02037",
+  // oldVAB: "0x5cBbA5484594598a660636eFb0A1AD953aFa4e32", // vab
+  // newVAB: "0x53BeF80E0EBE5A89dfb67782b12435aBeB943754", // exm
 
-  usdcAdress: "0x7493d25DAfE9dA7B73Ffd52F009D978E2415bE0c",
-  usdtAdress: "0x47719C2b2A6853d04213efc85075674E93D02037",
+  oldVAB: "0xed28b1890fbb4aa9ded528c1034fed278ff68f5d",
+  newVAB: "0xf27BeDc0C8a29DF0c4F91493EBa24814fF04D504",
 };
 
 // Defaults to e18 using amount * 10^18
