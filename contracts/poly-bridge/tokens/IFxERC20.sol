4c6d0dfa17b137da3479f6c5656e312e3dd3a38b
Mud <44410798+MudDev@users.noreply.github.com>
2024-01-03 22:21:21 -0700

    Remove faucet residue.

--- contracts/poly-bridge/tokens/IFxERC20.sol
+++ contracts/poly-bridge/tokens/IFxERC20.sol
@@ -9,7 +9,6 @@ interface IFxERC20 is IERC20 {
     function connectedToken() external returns (address);
 
     function initialize(
-        address owner,
         address _fxManager,
         address _connectedToken,
         string memory _name,
@@ -19,5 +18,5 @@ interface IFxERC20 is IERC20 {
 
     function mint(address user, uint256 amount) external;
 
-    function burn(address user, uint256 amount) external;    
+    function burn(address user, uint256 amount) external;
 }
