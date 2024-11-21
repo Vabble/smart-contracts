193be673b4697dee1ed253e7fcec36d2c67be42d
Mud <44410798+MudDev@users.noreply.github.com>
2024-11-21 16:32:40 -0700

    feature: update token name from VAB to VFX and adjust initial supply in Vabble contract

--- contracts/Ethereum/Vabble.sol
+++ contracts/Ethereum/Vabble.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Vabble is ERC20 {
-    constructor() ERC20("Vabble", "VAB") {
-        _mint(msg.sender, 1456250000 * 10 ** decimals());
+    constructor() ERC20("Vabble", "VFX") {
+        _mint(msg.sender, 145625000 * 10 ** decimals());
     }
 }
